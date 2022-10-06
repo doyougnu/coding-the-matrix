a353ebe5b6e02e44225f2af59627eced4ea45199
doyougnu <jeffrey.young@iohk.io>
2022-10-05 21:18:30 -0400

    into section 6.4 finally!

--- src/6.lisp
+++ src/6.lisp
@@ -21,4 +21,36 @@
   "Return the function that computes (f (g x))"
   #'(lambda (x) (funcall f (funcall g x))))
 
-;;; at pg 190
+;;; begin 6.4
+;;; its interesting that symbols can just be setf'd to whatever they need to be
+;;; and on't need to be declared first. How uncomfortably dynamic.
+;;; -----
+;;; just kidding I was confused about property lists which create maps from symbols
+
+(defun tree-search (states goal-p successors combiner)
+  "Find a state that satisfies goal-p. Start with states, and search according
+  to successors and combiner."
+  (dbg :search "~&;; Search: ~a" states)
+  (cond ((null states) fail)
+        ((funcall goal-p (first states)) (first states))
+        (t (tree-search
+            (funcall combiner
+                     (funcall successors (first states))
+                     (rest states))
+            goal-p successors combiner))))
+
+(defun depth-first-search (start goal-p successors)
+  "Search new states first until goal is reached."
+  (tree-search (list start) goal-p successors #'append))
+
+
+(defun binary-tree (x)
+  "And example binary tree"
+  (list (* 2 x) (+ 1 (* 2 x))))
+
+(defun is (value)
+  #'(lambda (x)
+      (eql x value)))
+
+(my-debug :search)
+(depth-first-search 1 (is 12) #'binary-tree)
